b751501f9031310a31ebdfab3a4c77cd2d7cac86
FZAdlani <fatimazahra.adlani@emsi-edu.ma>
2025-11-04 00:25:16 +0100

    L'ajout d'un paramètre à la méthode hello

--- src/main/java/ma/emsi/tp3fz_adlani/HelloResource.java
+++ src/main/java/ma/emsi/tp3fz_adlani/HelloResource.java
@@ -2,13 +2,16 @@ package ma.emsi.tp3fz_adlani;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 
 @Path("/hello-world")
 public class HelloResource {
+
     @GET
+    @Path("/personnes/{nom}")
     @Produces("text/plain")
-    public String hello() {
-        return "Hello, World!";
+    public String hello(@PathParam("nom") String nom) {
+        return "Hello, " + nom;
     }
 }
